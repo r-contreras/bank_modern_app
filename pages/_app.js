a576ad7202c2a6adae53abe12f3350e18c27d84f
r-contreras <rcontreras1905@gmail.com>
2022-08-27 12:03:32 -0600

    Safari fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import Script from 'next/script'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"
+        strategy="beforeInteractive"
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
